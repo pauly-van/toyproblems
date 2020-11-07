5e412abc46bedd0b16c504777c9be5458361cdab
Paul Nguyen <paulngu1120@gmail.com>
2020-11-07 10:53:03 +0000

    complete powerset

--- powerSet/powerSet.js
+++ powerSet/powerSet.js
@@ -16,6 +16,32 @@
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
+const outcomeOfABC = [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ];
 
 var powerSet = function(str) {
+    let result = [''];
+
+    const addToArray = function(pref, str){
+        if(str.length===0){
+            return [''];
+        }
+
+        for(let i=0;i<str.length;i++){
+            result.push(pref, str[i]);
+            addToArray(pref+str[i], str.slice(i+1));
+        }
+    }
+
+    addToArray('', str);
+    return result;
 };
+
+const assertEquals = function(actual, expect, desc){
+    if(actual === expect){
+        console.log(`Passed`, actual);
+    }else{
+        console.log(`Failed [${desc}]: Expected ${expect} but got ${actual}`);
+    }
+}
+
+assertEquals(powerSet('abc'), outcomeOfABC, 'Should provide powerSet of string');
